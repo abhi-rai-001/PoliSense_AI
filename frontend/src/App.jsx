4d18408848e61cbcc894442f2ac4ba71556375c6
Abhi Rai <raiabhinav182@gmail.com>
2025-07-19 23:02:44 +0530

    Resolved conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import UploadPage from "./pages/UploadPage";
 import ChatPage from "./pages/ChatPage";
+import Navbar from "./components/Navbar";
 
 
 
@@ -12,9 +13,9 @@ export default function App() {
     <Router>
       <Routes>
        
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/upload" element={<UploadPage />} />
-           <Route path="/chat" element={<ChatPage />} />
+        <Route path="/" element={<><Navbar/> <LandingPage /> </>} />
+        <Route path="/upload" element={<><Navbar/> <UploadPage /> </>} />
+        <Route path="/chat" element={<ChatPage />} />
 
       </Routes>
     </Router>
