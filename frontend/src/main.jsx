71133be85e989750a5e31cd8efd89bb873c96121
Abhi Rai <raiabhinav182@gmail.com>
2025-07-30 15:29:44 +0530

    Minor changes for clerk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} frontendApi="clerk.polisense.info">
     <App />
     </ClerkProvider>
   </React.StrictMode>
